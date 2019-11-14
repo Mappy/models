518d0750400aebab6732b32a5fe57306eeb12919
Tien TRAN-THUONG <>
2019-11-14 12:06:23 +0100

    - test model

--- research/object_detection/dataset_tools/create_mappy_blur_tf_record.py
+++ research/object_detection/dataset_tools/create_mappy_blur_tf_record.py
@@ -46,7 +46,7 @@ from object_detection.utils import label_map_util
 flags = tf.app.flags
 flags.DEFINE_string('data_dir', '', 'Root directory to raw pet dataset.')
 flags.DEFINE_string('output_dir', '', 'Path to directory to output TFRecords.')
-flags.DEFINE_string('label_map_path', 'data/pet_label_map.pbtxt',
+flags.DEFINE_string('label_map_path', 'data/mappy_blur_label_map.pbtxt',
                     'Path to label map proto')
 flags.DEFINE_boolean('faces_only', True, 'If True, generates bounding boxes '
                      'for pet faces.  Otherwise generates bounding boxes (as '
